ecc8687aed5ba64889bca2091e1a1b4da1660f62
Eva Keller <eva.keller1@gmail.com>
2020-12-04 16:36:29 +0100

    loop exercises

---
+++ 100greet3x.js
@@ -0,0 +1,16 @@
+//Write a loop that logs greeting three times.
+
+let greeting = 'Aloha!';
+for (let i = 1; i < 4; i++){
+    console.log(greeting);
+}
+
+/*
+let greeting = 'Aloha!';
+let count = 1;
+
+while (count <= 3) {
+  console.log(greeting); print greeting
+  count += 1;   check the count and do it if it hasn't reach the end of the counter
+}
+*/
---
+++ 100multiply.js
@@ -0,0 +1,17 @@
+//Write a for loop that iterates over all numbers from 1 to 100, 
+//and outputs the result of multiplying each element by 2.
+
+
+
+for ( let i = 1; i <= 100; i++) {
+    
+    let j = i * 2;
+    console.log(j);
+
+}
+
+/*
+for (let num = 1; num <= 100; num += 1) {
+  console.log(num * 2);
+}
+*/
